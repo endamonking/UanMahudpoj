62b40651f6840bdaf08625c97d653584041c9342
SHRIMP <pornpoj12@gmail.com>
2024-08-23 01:17:51 +0700

    [Uan] Create shuffle funtion
    Using Fisher-Yates shuffle algorithm

--- Assets/Script/Game_Manager.cs
+++ Assets/Script/Game_Manager.cs
@@ -20,8 +20,11 @@ public class Game_Manager : MonoBehaviour
             //add card to cardlist
             for (int a = 0; a < totalItem / 2; a++)
             {
-
+                int randomIndex = Random.Range(0, allCardlist.Count);
+                cardlist.Add(allCardlist[randomIndex]);
+                cardlist.Add(allCardlist[randomIndex]);
             }
+            ShuffleList(cardlist);
             for (int i =0; i < totalItem; i++)
             {
                 GameObject card = Instantiate(cardlist[Random.Range(0, cardlist.Count)], cardlist[0].transform.position, Quaternion.identity, cardContainer.transform);
@@ -36,4 +39,20 @@ public class Game_Manager : MonoBehaviour
     {
         
     }
+
+    //using Fisher-Yates shuffle algorithm to shuffle
+    private void ShuffleList<T>(List<T> list)
+    {
+        System.Random rng = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+
 }
