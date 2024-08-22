19cacfff328f2802fb66e88cc08ce4c197c92373
SHRIMP <pornpoj12@gmail.com>
2024-08-22 23:08:39 +0700

    [Uan] Making card Flipping
    Making card flipping via script

---
+++ Assets/Script/CardClicked.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class CardClicked : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject backCard;
+    [SerializeField]
+    private GameObject frontCard;
+
+    public int timer = 0;
+    public bool isFlipping = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (backCard.activeSelf != true)
+            backCard.SetActive(true);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void clicked()
+    {
+        if (!isFlipping)
+            startFlip();
+    }
+
+    private void startFlip()
+    {
+        Debug.Log("sFlip");
+        isFlipping = true;
+        StartCoroutine(flipping());
+    }
+
+    IEnumerator flipping()
+    {
+        //Rotating
+        for (int i = 0; i < 90; i++)
+        {
+            yield return new WaitForSeconds(0.01f);
+            transform.Rotate(new Vector3(0, -1, 0));
+            timer++;
+
+            if (timer >= 90)
+            {
+                Debug.Log("FLIP");
+            }
+        }
+        //Start shwoing front card
+        backCard.SetActive(false);
+        for (int i = 0; i < 90; i++)
+        {
+            yield return new WaitForSeconds(0.01f);
+            transform.Rotate(new Vector3(0, -1, 0));
+            timer++;
+
+            if (timer >= 180)
+            {
+                Debug.Log("FLIP");
+            }
+        }
+        timer = 0;
+        isFlipping = false;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+    }
+
+}
